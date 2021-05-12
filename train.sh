e8c59e96340bcbc628bce48d003ce7557df1f034
Di Chang <61009212+Boese0601@users.noreply.github.com>
2021-05-12 13:58:55 +0800

    Update train.sh

--- train.sh
+++ train.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
-MVS_TRAINING="/home/xyguo/dataset.ssd/dtu_mvs/processed/mvs_training/dtu/"
-python train.py --dataset=dtu_yao --batch_size=4 --trainpath=$MVS_TRAINING --trainlist lists/dtu/train.txt --testlist lists/dtu/test.txt --numdepth=192 --logdir ./checkpoints/d192 $@
+MVS_TRAINING="/home/ymz/MVSNet_pytorch-master/MVS_TRAINING/mvs_training/dtu"
+MVS_VALIDATING="/home/ymz/MVSNet_pytorch-master/MVS_TRAINING/mvs_training/dtu"
+python  train.py --mode train --dataset=dtu_yao --batch_size=1 --trainpath $MVS_TRAINING --trainlist lists/dtu/train.txt --testpath $MVS_VALIDATING --testlist lists/dtu/val.txt --numdepth=192 --logdir ./checkpoints/d192  --interval 1000 $@
+
