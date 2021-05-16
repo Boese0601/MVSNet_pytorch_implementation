4d584cd8cfbc919af1876883f6581c93e86310ed
Di Chang <61009212+Boese0601@users.noreply.github.com>
2021-05-16 12:05:07 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-MVS_TRAINING="/home/ymz/MVSNet_pytorch-master/MVS_TRAINING/mvs_training/dtu"
-python  train.py --mode test --dataset=dtu_yao --batch_size=4 --trainpath=$MVS_TRAINING --trainlist lists/dtu/train.txt --testlist lists/dtu/test.txt --numdepth=192 --logdir ./checkpoints/d192 --loadckpt /home/ymz/MVSNet_pytorch-master/checkpoints/model_000014.ckpt  $@
+MVS_TRAINING="./MVS_TRAINING/mvs_training/dtu"
+python  test.py --mode test --dataset=dtu_yao --batch_size=4 --trainpath=$MVS_TRAINING --trainlist lists/dtu/train.txt --testlist lists/dtu/test.txt --numdepth=256 --logdir ./checkpoints/testd256 --loadckpt ./checkpoints/d256/epoch_15/model_000015.ckpt  $@
